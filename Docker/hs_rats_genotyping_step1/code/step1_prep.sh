33f251013deae8b76995cd640c8d1c31c4f318c4
Deeeeen <675868892@qq.com>
2021-01-18 14:48:40 -0800

    docker step1 update

--- Docker/hs_rats_genotyping_step1/code/step1_prep.sh
+++ Docker/hs_rats_genotyping_step1/code/step1_prep.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-pipeline_arguments=/hs_genotyping_step1/data/pipeline_arguments
-dir_path=/hs_genotyping_step1/data/$(head -n 1 ${pipeline_arguments} | tail -n 1)
-original_sample_sheet=/hs_genotyping_step1/data/$(head -n 2 ${pipeline_arguments} | tail -n 1)
+
+pipeline_arguments=/pipeline/data/pipeline_arguments
+dir_path=/pipeline/data/$(head -n 1 ${pipeline_arguments} | tail -n 1)
+original_sample_sheet=$(head -n 2 ${pipeline_arguments} | tail -n 1)
+code=$(head -n 7 ${pipeline_arguments} | tail -n 1)
 #### read in arguments for the pipeline
 
+cd 
 ################################### Pre Demux ###################################
 #### This part constructs the directory structure.
 mkdir ${dir_path}
@@ -85,33 +88,12 @@ fi
 #### !!!!!!!!!!!!!!!!!!!!!!
 START=$(date +%s)
 #### This block handles the original sample sheet format.
-#### 1. remove double quotes
-#### 2. keep only hs rats data
-#### 3. duplicate the rfid column to be Sample_ID and Sample_Name
-#### 4. inplace the header row with new header row
-sed 's/\"//g' ${original_sample_sheet} | \
-awk -F',' '{ if($14 =="Heterogenous stock") { print } }' | \
-awk 'BEGIN{FS=OFS=","} {$1 = $5 OFS $5 OFS $1} 1' | \
-sed '1 i\
-Sample_ID,Sample_Name,runid,fastq_files,Library_ID,PCR_Barcode,rfid,Sample_Project,Sample_Barcode,filename,comments,flag,sex,coatcolor,organism,strain
-' > ${dir_path}/demux/sample_sheet.csv
-sample_sheet=${dir_path}/demux/sample_sheet.csv
-
-#### The for loop separates out the flow cell by Library_ID
-#### and create separated sample sheet for each Library_ID
-for n in $(cut -d "," -f5,6 < ${sample_sheet} | grep -v "Library_ID" | sort| uniq)
-do
-    library_id=$(cut -d "," -f1 <<< ${n})
-    pcr_barcode=$(cut -d "," -f2 <<< ${n})
-    full_run_id=$(echo ${dir_path} | rev | cut -d "/" -f1 | rev)
-    if [ -f "${dir_path}/demux/SampleSheet_${pcr_barcode}_${library_id}_${full_run_id}.csv" ]; then
-        rm ${dir_path}/demux/SampleSheet_${pcr_barcode}_${library_id}_${full_run_id}.csv
-    fi
-    head -1 ${sample_sheet} > ${dir_path}/demux/SampleSheet_${pcr_barcode}_${library_id}_${full_run_id}.csv
-    awk -v pcr_barcode=$pcr_barcode -v library_id=$library_id -F ',' \
-    '{ if($6 == pcr_barcode && $5 == library_id){ print } }' ${sample_sheet} >> ${dir_path}/demux/SampleSheet_${pcr_barcode}_${library_id}_${full_run_id}.csv
-    #### extract the corresponding sample barcode metadata
-done
+#### extract the corresponding sample barcode metadata
+source activate genotyping
+python3 ${code}/separate_metadata.py \
+    ${original_sample_sheet} \
+    ${dir_path}/demux
+conda deactivate
 
 while [ "$(jobs -rp | wc -l)" -gt 0 ]; do
    sleep 60
