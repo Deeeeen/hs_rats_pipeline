631d6399eb224dc52d4836a32fd85827baa04146
Deeeeen <675868892@qq.com>
2021-03-11 14:21:08 -0800

    minor fix

--- TSCC/quality_control/util/stitch_info_score.r
+++ TSCC/quality_control/util/stitch_info_score.r
@@ -9,5 +9,5 @@ info_score_hwe <- read.table(data_path, header=FALSE,  strip.white=TRUE,sep="\t"
 colnames(info_score_hwe)<-c("pos","info_score")
 
 png(paste0(out_path, "/stitch_info_score_histo.png"))
-hist(info_score_hwe$info_score,  breaks = 100, xlab="Info Score", ylab="Frequency", main="1536 HS Rats Info Score")
+hist(info_score_hwe$info_score,  breaks = 100, xlab="Info Score", ylab="Frequency")
 dev.off()
